ed9720dd1bb4ab0a6e9af6f14c97c78664401285
travisbrigman <travis1000@gmail.com>
2021-07-22 09:48:04 -0500

    Initial Commit

---
+++ FetchNow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FetchNow
+//
+//  Created by Travis Brigman on 7/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
